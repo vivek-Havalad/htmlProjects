69b37f69c8796aa2fabc611a5fe389960b62e95d
vivek-Havalad <vivek.hv20@gmail.com>
2016-12-01 19:07:47 +0530

    1st

---
+++ 3.js
@@ -0,0 +1,31 @@
+function myfun()
+{
+  var num1=document.forms["cal"]["fir"].value;
+  var num2=document.forms["cal"]["sec"].value;;
+  var choice= document.forms["cal"]["option"].value;
+var num1=parseInt(num1);
+var num2=parseInt(num2);
+  var result=0;
+  switch (choice) {
+    case "add":result=num1+num2;
+      break;
+case "subt":result=num1-num2;
+
+  break;
+  case "mul":result=num1*num2;
+
+    break;
+    case "div":if (num2 == 0) {
+      document.getElementById("result").value="infinity";
+      return;
+    }
+    else {
+      result=num1/num2;
+    }
+
+      break;
+    default:result="no choice";
+
+  }
+  document.getElementById("result").value=result;
+}
